2ff50c7f546a059a2d15d7751b8f525a8bd2a3cf
Nattapong Unaregul <tylerlantern@gmail.com>
2022-01-19 13:35:11 +0700

    add audio label text diplay current progress

--- Drawing/AudioPlayerView.swift
+++ Drawing/AudioPlayerView.swift
@@ -4,43 +4,58 @@ import CoreGraphics
 
 public enum AudioPlayerState {
   case readyToPlay,
-  loading,
-  pause
+       loading,
+       pause
   
 }
 
 public struct AudioPlayerView: View {
-  @State public var progress : Double = 0
-  @State public var lenghtOfVideosInSeconds : Int = 60
+  //MARK:- Progresss between 0 and 1
+//  @State public var lenghtOfVideosInSeconds : Int = 60
   
-  private var progressInSeconds : Binding<Double> { Binding (
-      get: { progress *  Double(lenghtOfVideosInSeconds) },
-      set: { _ in  }
-      )
+  @Binding var progressInSeconds : Double
+  var lenghtOfVideosInSeconds : Int
+  @State var displayForward : String = ""
+  @State var displayBackward : String = ""
+  
+  public init(progressInSeconds : Binding<Double> , lenghtOfVideosInSeconds :  Int) {
+    _progressInSeconds = progressInSeconds
+    self.lenghtOfVideosInSeconds = lenghtOfVideosInSeconds
   }
   
-  public func displayCurrentProgress() {
-    
+  private func fraction(_ s : Int) -> (Int,Int,Int) {
+    return (s / 3600, (s % 3600) / 60, (s % 3600) % 60)
   }
   
-  public func displayRemainingTime() {
-    
+  public func calDisplayForward(_ s : Double) -> String {
+    let (_, minutes,seconds) = fraction(Int(floor(s)))
+    return String(format: "%02d:%02d", minutes,seconds)
   }
   
- public var body: some View {
+  public func calDisplayBackward(_ s : Double) -> String {
+    let remainingSeconds = Int(floor(Double(lenghtOfVideosInSeconds) - s))
+    let (_, minutes,seconds) = fraction(remainingSeconds)
+    return String(format: "%02d:%02d", minutes,seconds)
+  }
+  
+  public var body: some View {
     GeometryReader {proxy in
       ZStack {
         Capsule().fill(Color.gray)
-        HStack() {
+        HStack(spacing: 2) {
           PlayPauseLoadingIcon()
             .frame(width: min(proxy.size.width,proxy.size.height) ,
                    height: min(proxy.size.width,proxy.size.height))
-          Text("00:00")
+          Text(displayForward).frame(width:50)
           VStack {
-            AudioProgressBarSlider(value: progressInSeconds, in: 0...Double(lenghtOfVideosInSeconds), step: 1)
-            .background(Color.blue)
+            AudioProgressBarSlider(value: $progressInSeconds, in: 0...Double(lenghtOfVideosInSeconds), step: 1)
+              .onChange(of: progressInSeconds, perform: { newValue in
+                self.displayForward = calDisplayForward(newValue)
+                self.displayBackward = calDisplayBackward(newValue)
+              })
+              .background(Color.blue)
           }
-          Text("00:00")
+          Text(displayBackward).frame(width:50)
           Spacer()
         }
       }
@@ -69,7 +84,7 @@ struct CircleAudioPlayer: Shape {
     var p = Path()
     let padding : CGFloat = 12
     let diameter = min(rect.width,rect.height) - padding
-
+    
     p.addArc(
       center: CGPoint(x: padding / 2 + diameter / 2, y: diameter / 2 + padding / 2),
       radius: diameter / 2,
@@ -87,7 +102,6 @@ struct Playicon : Shape {
     let min = min(rect.width,rect.height)
     let padding : CGFloat = min * 0.35
     let diameter = min - padding
-//    let center = CGPoint(x: padding / 2 + diameter / 2, y: diameter / 2 + padding / 2)
     p.move(to: .init(x: padding, y: rect.minY + padding))
     p.addLine(to: .init(x: diameter, y: rect.midY))
     p.addLine(to: .init(x: padding, y: rect.maxY - padding))
@@ -98,10 +112,10 @@ struct Playicon : Shape {
 }
 
 struct AudioPlayerView_Previews: PreviewProvider {
-    static var previews: some View {
-      AudioPlayerView.init()
-        .frame(height:55)
-    }
+  static var previews: some View {
+    AudioPlayerView.init(progressInSeconds: .constant(20), lenghtOfVideosInSeconds: 60)
+      .frame(height:55)
+  }
 }
 
 
--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      AudioPlayerView.init()
-        .frame(height: 55)
-    }
+  
+  @State var progressInSeconds : Double = 0
+  
+  var body: some View {
+    AudioPlayerView(progressInSeconds: $progressInSeconds, lenghtOfVideosInSeconds: 300)
+      .frame(height: 55)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
